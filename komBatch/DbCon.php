46850b04e1fa11e6b6a3b22141f4a6c0f9315398
IamJoseph <Lostintheuniverse0@gmail.com>
2015-07-08 00:48:36 -0400

    Added content images and PHP file
    The three images that are loaded according to the date selected on the
    form and the database connection file

---
+++ komBatch/DbCon.php
@@ -0,0 +1,40 @@
+<?php
+	namespace komBatch;
+    class DbCon {        
+        protected static $connection;
+        public function connect() {    
+            if(!isset(self::$connection)) {
+                $config = parse_ini_file('/xampp/htdocs/config.ini');
+                self::$connection = new \mysqli('127.0.0.1',$config['username'],$config['password'],$config['dbname']); //needs the \ before mysqli for whatever reason
+            }            
+            if(self::$connection === false) {
+                return false;
+            }
+            return self::$connection;
+        } 
+        public function query($query) {
+			$connection = $this -> connect();
+            $result = $connection -> query($query);
+            return $result;
+        } 
+        public function select($query) {
+            $rows = array();
+            $result = $this -> query($query);
+            if($result === false) {
+                return false;
+            }
+            while ($row = $result -> fetch_assoc()) {
+                $rows[] = $row;
+            }
+            return $rows;
+        }      
+        public function error() {
+            $connection = $this -> connect();
+            return $connection -> error;
+        }      
+        public function quote($value) {
+            $connection = $this -> connect();
+            return "'" . $connection -> real_escape_string($value) . "'";
+        }
+    }
+?>
